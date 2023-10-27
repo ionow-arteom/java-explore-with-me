3d7bf70ee18fcd85d99fc17898ac03f21d5c403f
ionow-arteom <ionow.arteom@yandex.ru>
2023-10-27 12:11:39 +0600

    кодстайлkkk1

--- ewm-service/src/main/java/ru/practicum/compilation/CompilationRepository.java
+++ ewm-service/src/main/java/ru/practicum/compilation/CompilationRepository.java
@@ -2,9 +2,11 @@ package ru.practicum.compilation;
 
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface CompilationRepository extends JpaRepository<Compilation, Long> {
 
     List<Compilation> findByPinned(Boolean pinned, PageRequest pageRequest);
--- ewm-service/src/main/java/ru/practicum/request/RequestRepository.java
+++ ewm-service/src/main/java/ru/practicum/request/RequestRepository.java
@@ -1,11 +1,13 @@
 package ru.practicum.request;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import ru.practicum.util.enumerated.Status;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface RequestRepository extends JpaRepository<Request, Long> {
 
     List<Request> findAllByRequesterId(Long userId);
