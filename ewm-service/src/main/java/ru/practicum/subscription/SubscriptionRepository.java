8fdeab287a747eed2cd7ffffbde00e4e18407f33
ionow-arteom <ionow.arteom@yandex.ru>
2023-12-11 18:26:28 +0600

    add subscription

--- ewm-service/src/main/java/ru/practicum/subscription/SubscriptionRepository.java
+++ ewm-service/src/main/java/ru/practicum/subscription/SubscriptionRepository.java
@@ -6,6 +6,8 @@ import java.util.List;
 
 public interface SubscriptionRepository extends JpaRepository<Subscription, Long> {
     List<Subscription> findBySubscriberId(Long subscriberId);
+
     boolean existsBySubscriberIdAndSubscribedToId(Long subscriberId, Long subscribedToId);
+
     void deleteBySubscriberIdAndSubscribedToId(Long subscriberId, Long subscribedToId);
 }
