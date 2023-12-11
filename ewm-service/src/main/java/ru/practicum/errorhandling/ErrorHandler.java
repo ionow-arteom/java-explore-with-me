767f4db869074c9d55546eb84155a10881342ccf
ionow-arteom <ionow.arteom@yandex.ru>
2023-12-11 13:20:51 +0600

    review recommendations

--- ewm-service/src/main/java/ru/practicum/errorhandling/ErrorHandler.java
+++ ewm-service/src/main/java/ru/practicum/errorhandling/ErrorHandler.java
@@ -1,6 +1,8 @@
 package ru.practicum.errorhandling;
 
 import org.hibernate.exception.ConstraintViolationException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -10,6 +12,8 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class ErrorHandler {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(ErrorHandler.class);
+
     /**
      * Handles the exceptions of type NotFoundException.
      * @param e Exception to be handled.
@@ -18,6 +22,7 @@ public class ErrorHandler {
     @ExceptionHandler
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public ErrorResponse handleNotFoundException(final NotFoundException e) {
+        LOGGER.error("NotFoundException: {}", e.getMessage());
         return new ErrorResponse(e.getMessage());
     }
 
@@ -29,6 +34,7 @@ public class ErrorHandler {
     @ExceptionHandler
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorResponse handleValidationException(final ValidationException e) {
+        LOGGER.error("ValidationException: {}", e.getMessage());
         return new ErrorResponse(e.getMessage());
     }
 
@@ -40,6 +46,7 @@ public class ErrorHandler {
     @ExceptionHandler
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorResponse handleMethodArgumentNotValid(final MethodArgumentNotValidException e) {
+        LOGGER.error("MethodArgumentNotValidException: {}", e.getMessage());
         return new ErrorResponse(e.getMessage());
     }
 
@@ -51,6 +58,7 @@ public class ErrorHandler {
     @ExceptionHandler
     @ResponseStatus(HttpStatus.CONFLICT)
     public ErrorResponse handleConstraintViolation(final ConstraintViolationException e) {
+        LOGGER.error("ConstraintViolationException: {}", e.getMessage());
         return new ErrorResponse(e.getMessage());
     }
 
@@ -62,6 +70,19 @@ public class ErrorHandler {
     @ExceptionHandler
     @ResponseStatus(HttpStatus.CONFLICT)
     public ErrorResponse handleConflictException(final ConflictException e) {
+        LOGGER.error("ConflictException: {}", e.getMessage());
         return new ErrorResponse(e.getMessage());
+    }
+
+    /**
+     * Handles unhandled exceptions.
+     * @param e Exception to be handled.
+     * @return ErrorResponse containing the error message.
+     */
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ErrorResponse handleUnhandledException(final Exception e) {
+        LOGGER.error("Unhandled Exception: {}", e.getMessage(), e);
+        return new ErrorResponse("Internal Server Error");
     }
 }
